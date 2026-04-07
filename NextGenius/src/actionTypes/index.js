45c227378505fed5be26cadaffab5c555e8bbfdd
fintie <fintie@users.noreply.github.com>
2026-04-07 12:05:39 +0000

    deploy: 6b2606adf5d09121a48533ff229785d064773b6b

--- NextGenius/src/actionTypes/index.js
+++
@@ -1,9 +0,0 @@
-// ------------------------------------
-// Fetch Actions
-// ------------------------------------
-export const FETCH_REQUEST = 'FETCH_REQUEST';
-export const FETCH_SUCCESS = 'FETCH_SUCCESS';
-export const FETCH_FAILURE = 'FETCH_FAILURE';
-
-// Get Team Data
-export const GET_TEAM_DATA = 'GET_TEAM_DATA';
